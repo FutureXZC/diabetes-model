cbc8156d452d354cbf92d1046c712c20ff772cb4
FutureXZC <1073739700@qq.com>
2021-03-06 20:25:15 +0800

    [+] 完善病情预测表单

--- frontend/src/api/analysis.js
+++ frontend/src/api/analysis.js
@@ -1,16 +1,30 @@
 import request from '@/utils/request'
 
-export function analysis(formData) {
+
+export function analysisDesc(formData) {
   return request({
-    url: 'http://127.0.0.1:3000/process/analysis',
+    url: 'http://127.0.0.1:3000/process/analysisDesc',
     method: 'post',
     params: formData,
-    // transformRequest:[function(){
-    //   return JSON.stringify(formData)
-    // }],
-    // headers: {
-    //   'Content-Type':'application/json;'
-    // },
-    // formData
+  })
+}
+
+export function analysisExam(formData) {
+  return request({
+    url: 'http://127.0.0.1:3000/process/analysisExam',
+    method: 'post',
+    params: formData,
+  })
+}
+
+export function analysisDouble(formData) {
+  return request({
+    url: 'http://127.0.0.1:3000/process/analysisDouble',
+    method: 'post',
+    params: formData,
+    body: JSON.stringify(formData),
+    headers: {
+      'Content-Type': 'application/json'
+    }
   })
 }
--- frontend/src/views/predict/index.vue
+++ frontend/src/views/predict/index.vue
@@ -1,9 +1,44 @@
 <template>
   <div class="predict-wrap">
-    <el-form :model="form" ref="form" :inline="true" label-width="80px">
+    <el-form :model="form" ref="form" :inline="true" label-width="125px">
 
       <el-row>
-        <el-form-item label="症状描述" :prop="form.desc">
+        <el-form-item label="姓名" :rules="[ { required: true } ]">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+      </el-row>
+
+      <el-row>
+        <el-form-item label="性别" :rules="[ { required: true } ]">
+          <el-radio-group v-model="form.sex" :disabled="isDisabled">
+            <el-radio-button label="男"></el-radio-button>
+            <el-radio-button label="女"></el-radio-button>
+          </el-radio-group>
+        </el-form-item>
+      </el-row>
+
+      <el-row>  
+        <el-form-item label="年龄" :rules="[ { required: true } ]">
+          <el-input-number 
+            v-model="form.age"
+            :step="1" 
+            :max="110"
+            :disabled="isDisabled"
+          ></el-input-number>
+        </el-form-item>
+      </el-row>
+
+      <el-row>
+        <el-form-item label="是否填写症状描述" :prop="isDesc">
+          <el-radio-group v-model="isDesc" :disabled="isDescDisable">
+            <el-radio-button label="是"></el-radio-button>
+            <el-radio-button label="否"></el-radio-button>
+          </el-radio-group>
+        </el-form-item>
+      </el-row>
+
+      <el-row>
+        <el-form-item label="症状描述" :prop="form.desc" v-show="isDescShow">
           <el-input 
             type="textarea" 
             v-model="form.desc" 
@@ -16,14 +51,23 @@
       </el-row>
 
       <el-row>
-        <el-form-item v-for="(item,i) in metrics" :label="item[0]" :prop="item[1]">
-          <el-input-number 
-            v-model="form[item[1]]" 
+        <el-form-item label="是否填写体检数据" :prop="isExam">
+          <el-radio-group v-model="isExam" :disabled="isExamDisable">
+            <el-radio-button label="是"></el-radio-button>
+            <el-radio-button label="否"></el-radio-button>
+          </el-radio-group>
+        </el-form-item>
+      </el-row>
+
+      <el-row>
+        <el-form-item v-for="(item,i) in metrics" :label="item[0]" :prop="item[1]"  v-show="isExamShow">
+          <div class="metrics"><el-input-number 
+            v-model="form['exam'][item[1]]" 
             :precision="2" 
             :step="0.1" 
             :max="10"
             :disabled="isDisabled"
-          ></el-input-number>
+          ></el-input-number></div>
         </el-form-item>
       </el-row>
 
@@ -39,46 +83,94 @@
 </template>
 
 <script>
-import { analysis } from '@/api/analysis'
+import { analysisDouble, analysisExam, analysisDesc } from '@/api/analysis'
 
 export default {
   data() {
     return {
       isDisabled: false,
+      isDesc: '是',
+      isExam: '是',
       metrics: [
-        ['体检指标1', 'm1'],
-        ['体检指标2', 'm2'],
-        ['体检指标3', 'm3'],
-        ['体检指标4', 'm4'],
-        ['体检指标5', 'm5'],
-        ['体检指标1', 'm1'],
-        ['体检指标2', 'm2'],
-        ['体检指标3', 'm3'],
-        ['体检指标4', 'm4'],
-        ['体检指标5', 'm5'],
-        ['体检指标1', 'm1'],
-        ['体检指标2', 'm2'],
-        ['体检指标3', 'm3'],
-        ['体检指标4', 'm4'],
-        ['体检指标5', 'm5'],
-        ['体检指标1', 'm1'],
-        ['体检指标2', 'm2'],
-        ['体检指标3', 'm3'],
-        ['体检指标4', 'm4'],
-        ['体检指标5', 'm5'],
-        ['体检指标1', 'm1'],
-        ['体检指标2', 'm2'],
-        ['体检指标3', 'm3'],
-        ['体检指标4', 'm4'],
+        ['糖化血红蛋白', 'thxhdb'],
+        ['γ-谷氨酰转肽酶', 'gaxztm'],
+        ['乳酸脱氢酶', 'rstqm'],
+        ['低密度脂蛋白', 'dmdzdb'],
+        ['前白蛋白', 'qbdb'],
+        ['尿素', 'ns1'],
+        ['尿酸', 'ns2'],
+        ['总胆固醇', 'zdgc'],
+        ['总胆汁酸', 'zdzs'],
+        ['总胆红素', 'zdhs'],
+        ['总蛋白', 'zdb'],
+        ['球蛋白', 'qdb'],
+        ['甘油三脂', 'gysz'],
+        ['白球比', 'bqb'],
+        ['白蛋白', 'bdb'],
+        ['直接胆红素', 'zjdhs'],
+        ['碱性磷酸酶', 'jxlsm'],
+        ['肌酐', 'jg'],
+        ['肌酸激酶', 'jsjm'],
+        ['胆碱脂酶', 'djzm'],
+        ['脂蛋白(a)', 'zdba'],
+        ['葡萄糖', 'ptt'],
+        ['载脂蛋白A1', 'zzdba1'],
+        ['载脂蛋白B', 'zzdbb'],
+        ['间接胆红素', 'jjdhs'],
+        ['高密度脂蛋白', 'gmdzdb']
       ],
       form: {
-        m1: '',
-        m2: '',
-        m3: '',
-        m4: '',
-        m5: '',
-        desc: ''
+        name: '',
+        sex: '男',
+        age: '',
+        desc: '',
+        exam: {
+          thxhdb: '',
+          gaxztm: '',
+          rstqm: '',
+          dmdzdb: '',
+          qbdb: '',
+          ns1: '',
+          ns2: '',
+          zdgc: '',
+          zdzs: '',
+          zdhs: '',
+          zdb: '',
+          qdb: '',
+          gysz: '',
+          bqb: '',
+          bdb: '',
+          zjdhs: '',
+          jxlsm: '',
+          jg: '',
+          jsjm: '',
+          djzm: '',
+          zdba: '',
+          ptt: '',
+          zzdba1: '',
+          zzdbb: '',
+          jjdhs: '',
+          gmdzdb: ''
+        }
       }
+    }
+  },
+
+  computed: {
+    isDescShow () {
+      return this.isDesc == '是'
+    },
+
+    isExamShow () {
+      return this.isExam == '是'
+    },
+
+    isDescDisable () {
+      return this.isExam == '否'
+    },
+
+    isExamDisable () {
+      return this.isDesc == '否'
     }
   },
 
@@ -97,19 +189,67 @@ export default {
       for (let key in this.form) {
         formData[key] = this.form[key]
       }
-      // console.log(formData);
-      analysis(formData).then(res => {
-        // console.log(res.data)
-        let msg = '该症状下患糖尿病的概率为<strong><i>' + res.data + '<i></strong>。'
-        this.$alert(msg, '判定结果', {
-          dangerouslyUseHTMLString: true
-        });
-        this.isDisabled = false
-      })
+      console.log(formData);
+      if (this.isExam == '是' && this.isDesc == '是') {
+        // analysisDouble(formData).then(res => {
+        //   console.log(res.data)
+        //   let msg = '该症状下患糖尿病的概率为<strong><i>' + res.data + '<i></strong>。'
+        //   this.$alert(msg, '判定结果', {
+        //     dangerouslyUseHTMLString: true
+        //   });
+        //   this.isDisabled = false
+        // })
+        let _this = this
+        fetch('http://127.0.0.1:3000/process/analysisDouble', {
+          method: 'post',
+          body: JSON.stringify(formData),
+          headers: { 'Content-Type': 'application/json' }
+        }).then(res => { return res.json(); }).then(res => {
+          console.log(res)
+          let msg = '根据体检数据和症状描述分析，患者患糖尿病的概率为<strong><i>' + res + '<i></strong>。'
+          _this.$alert(msg, '判定结果', {
+            dangerouslyUseHTMLString: true
+          });
+          _this.isDisabled = false
+        })
+      } else if (this.isDesc == '是') {
+        fetch('http://127.0.0.1:3000/process/analysisDesc', {
+          method: 'post',
+          body: JSON.stringify(formData),
+          headers: { 'Content-Type': 'application/json' }
+        }).then(res => { return res.json(); }).then(res => {
+          console.log(res)
+          let msg = '根据症状描述分析，患者患糖尿病的概率为<strong><i>' + res + '<i></strong>。'
+          _this.$alert(msg, '判定结果', {
+            dangerouslyUseHTMLString: true
+          });
+          _this.isDisabled = false
+        })
+      } else {
+        fetch('http://127.0.0.1:3000/process/analysisExam', {
+          method: 'post',
+          body: JSON.stringify(formData),
+          headers: { 'Content-Type': 'application/json' }
+        }).then(res => { return res.json(); }).then(res => {
+          console.log(res)
+          let msg = '根据体检数据分析，患者患糖尿病的概率为<strong><i>' + res + '<i></strong>。'
+          _this.$alert(msg, '判定结果', {
+            dangerouslyUseHTMLString: true
+          });
+          _this.isDisabled = false
+        })
+      }
     },
+
     resetForm(formName) {
       this.form.desc = ''
+      this.form.age = 0
+      this.form.name = ''
+      this.form.sex = '男'
+      this.isDesc = '是'
+      this.isExam = '是'
       this.$refs[formName].resetFields();
+      console.log(this)
     }
   }
 }
@@ -120,6 +260,9 @@ export default {
     padding: 20px;
   }
   .desc {
-    width: 500px;
+    width: 515px;
+  }
+  .metrics {
+    margin-right: 35px;
   }
 </style>
