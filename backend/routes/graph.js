2800511f806fcf3c3b4845abafde1ad46b806aa3
FutureXZC <1073739700@qq.com>
2021-03-09 17:10:16 +0800

    [*] 删除冗余信息

--- backend/routes/graph.js
+++ backend/routes/graph.js
@@ -1,9 +1,6 @@
 var express = require('express')
 var router = express.Router()
-var child_process = require('child_process')
-var fs = require('fs')
-  
-// 调用本地模型，使用当前时间戳作为文档的标题作为数据文件夹名称
+
 router.get('/', (req, res) => {
   res.sendFile(process.cwd() + '\\src\\graph\\map.html')
 })
